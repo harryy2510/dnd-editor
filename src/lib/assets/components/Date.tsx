a7010c0e2b899dc9bde7bc89c0d574d00f3b51a0
Tanmay Vijayvargiya <tanmayvijayvargiya@pop-os.localdomain>
2021-01-02 12:44:38 +0530

    fix: adding missing date component

---
+++ src/lib/assets/components/Date.tsx
@@ -0,0 +1,121 @@
+import React from 'react'
+import PubSub from '@harryy/pubsub'
+import { Trans } from '@lingui/macro'
+import { TextField } from '@material-ui/core'
+import * as yup from 'yup'
+import { DndComponentItem, RenderProps } from '../../types'
+import { FormikValues, useFormikContext } from 'formik'
+import { getFromikProps, getComponentState, useValidations } from '../../utils'
+import { get, noop } from 'lodash-es'
+import { DatePicker } from '@material-ui/pickers'
+
+export default {
+    render: (renderProps: RenderProps, id: string, formKey: string) => {
+        const handleClick = (ev: React.MouseEvent) => {
+            ev.preventDefault()
+            PubSub.publish('component/click', { type: 'form-elements', data: id })
+        }
+        const state = getComponentState(renderProps, id)
+        const labelText = `${state?.question}${state?.required ? '*' : ''}`
+        let formikProps: any = {}
+        const formik = useFormikContext<FormikValues>()
+        if (formik && formKey) {
+            formikProps = getFromikProps(formKey, formik, (value: Date) => {
+                return {
+                    text: value.toISOString(),
+                    valueType: 'String'
+                }
+            })
+            formikProps.helperText = formikProps.helperText?.text || state?.hint
+            if (Array.isArray(formikProps.value)) {
+                formik.setFieldValue(formKey, formikProps.value[0])
+            }
+            formikProps.value = formikProps.value?.text
+                ? new Date(formikProps.value?.text)
+                : new Date()
+            formikProps.helperText = formikProps.helperText?.text || state?.hint
+            formikProps.onChange = (date: Date) =>
+                formik.setFieldValue(formKey, { text: date.toISOString(), valueType: 'String' })
+        }
+        return (
+            <div onClick={handleClick}>
+                <DatePicker
+                    label={labelText}
+                    value={formikProps.value}
+                    onChange={formikProps.onChange || noop}
+                    renderInput={(props) => (
+                        <TextField
+                            {...formikProps}
+                            {...props}
+                            fullWidth
+                            variant="outlined"
+                            label={labelText}
+                        />
+                    )}
+                />
+            </div>
+        )
+    },
+    export: () => {
+        return ''
+    },
+    initialValues: {
+        question: 'Question',
+        placeholder: 'Placeholder',
+        hint: 'Optional Hint',
+        inputType: 'text',
+        multiline: false,
+        characterLimit: '12',
+        pii: '',
+        className: '',
+        required: true,
+        enabled: true,
+        grid: 12,
+        itemType: 'Input',
+        validation: { key: 'none' },
+        style: {
+            textAlign: 'left'
+        }
+    },
+    settings: [
+        { id: 'question', type: 'labeledTextInput', grid: 12, label: <Trans>Question</Trans> },
+        {
+            id: 'placeholder',
+            type: 'labeledTextInput',
+            grid: 12,
+            label: <Trans>Custom Placeholder</Trans>
+        },
+        { id: 'hint', type: 'labeledTextInput', grid: 12, label: <Trans>Hint</Trans> },
+        {
+            id: 'characterLimit',
+            type: 'labeledNumberInput',
+            grid: 12,
+            label: <Trans>Character limit</Trans>
+        },
+        { id: 'pii', type: 'labeledTextInput', grid: 12, label: <Trans>PII</Trans> },
+        { id: 'validation', type: 'inputValidation', grid: 12, label: <Trans>Validation</Trans> },
+        {
+            id: 'defaultValue',
+            type: 'labeledTextInput',
+            grid: 12,
+            label: <Trans>Default value</Trans>
+        },
+        { id: 'className', type: 'labeledTextInput', grid: 12, label: <Trans>Class name</Trans> },
+        { id: 'required', type: 'labeledSwitch', grid: 12, label: <Trans>Required</Trans> },
+        { id: 'enabled', type: 'labeledSwitch', grid: 12, label: <Trans>Enabled</Trans> }
+    ],
+    validationSchema: (renderProps: RenderProps, id: string, parentSchema) => {
+        const state = getComponentState(renderProps, id)
+        const { validations } = useValidations()
+        const validation = get(validations, state?.validation.key)
+        let schema =
+            validation?.validation?.(validation.toString(state?.validation.formValue)) ||
+            parentSchema ||
+            yup.string()
+        schema = state?.required ? schema.required('Required field') : schema
+        schema = state?.characterLimit
+            ? schema.max(state?.characterLimit, `Character limit is ${state?.characterLimit}`)
+            : schema
+        return yup.object().shape({ text: schema })
+    }
+} as DndComponentItem
