0ead465df80bf428f990231f5cbb2727d82e620c
Anuj <anuj.g@appointy.com>
2021-03-02 16:26:21 +0530

    feat: default font family

--- src/lib/components/preferences/items/FontFamily.tsx
+++ src/lib/components/preferences/items/FontFamily.tsx
@@ -1,9 +1,34 @@
 import { Trans } from '@lingui/macro'
+import { Theme } from '@material-ui/core'
+import { useTheme } from '@material-ui/styles'
 import React from 'react'
+import { useDndEditorContext } from '../../../DndEditorProvider'
+import { DndState } from '../../../types'
 import { useFonts } from '../../../utils'
 import Dropdown, { DropdownOption, DropdownProps } from '../components/Dropdown'
 
+const useFontFamily = (value: string) => {
+    const { template, state } = useDndEditorContext()
+    const { typography } = useTheme<Theme>()
+
+    if (value) {
+        return value
+    }
+
+    const templateFontFamily = Object.values(state.entities[template.id].values).reduce(
+        (p, n) => ({ ...p, ...n.style }),
+        {}
+    ).fontFamily
+
+    if (templateFontFamily) {
+        return templateFontFamily
+    }
+
+    return typography.fontFamily
+}
+
 const FontFamily: React.FC<DropdownProps> = (props) => {
+    const value = useFontFamily(props.value)
     const { fontFamily } = useFonts()
     const options: DropdownOption[] = fontFamily.map((family) => ({
         ...family,
@@ -12,7 +37,10 @@ const FontFamily: React.FC<DropdownProps> = (props) => {
             fontFamily: `${family.id}, sans-serif`
         }
     }))
-    return <Dropdown label={<Trans>Font Family</Trans>} {...props} options={options} />
+
+    return (
+        <Dropdown label={<Trans>Font Family</Trans>} {...props} options={options} value={value} />
+    )
 }
 
 export default FontFamily
